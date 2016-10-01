9a0174c6e1028165988e32f3bf236d3b405ceb19
Dimitri Belopopsky <dimitri@belopopsky.com>
2016-10-01 22:18:13 +0200

    subprocess test

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import time
 import generate_page
-
+import subprocess
 
 def main():
 
+    subprocess.call(["chromium", "--kiosk", "index.html", "--incognito"])
     while True:
         generate_page.main()
         time.sleep(3500)  # update every 5 minutes
