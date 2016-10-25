028305f976d4a87a07a4cc34813726144cfed700
Dimitri Belopopsky <dimitri@belopopsky.com>
2016-10-25 12:51:33 +0200

    Added automatic removal of images

--- slideshow.py
+++ slideshow.py
@@ -1,6 +1,9 @@
+#! python
+
 # Script by Dimitri Belopopsky
 # Requires https://github.com/ShadowMitia/SimpleSlideshow
 # Put this script in the same folder as the SimpleSlideshow app
+
 
 import time
 import generate_page
@@ -13,30 +16,37 @@ url_concierge = "http://concierge.la-faps.fr/week_all.php?area=1&pview=1"
 chrome = os.path.join("C:\Program Files","Google","Chrome","Application","chrome.exe")
 firefox = os.path.join("C:\Program Files","Mozilla Firefox","firefox.exe")
 path_to_drive = os.path.join(os.path.expanduser("~"), "Google Drive", "Affiches")
-# path_to_drive = os.path.join(os.path.expanduser("~"), "Dropbox", "images")
 
 def copy_images(path_src, path_dst):
-    print("toto: " +path_src)
     for el in os.listdir(path_src):
+        print(el)
         if el.split(".")[-1] in generate_page.authorized_extensions:
             src = os.path.join(path_src, el)
             dst = os.path.join(path_dst, el)
             copy(src, dst)
         else:
             dir_src = os.path.join(path_src, el)
+			
+def remove_images(path_src, path_dst):
+    files = os.listdir(path_src)
+    for el in os.listdir(path_dst):
+        if el.split(".")[-1] in generate_page.authorized_extensions and el not in files:
+            print("Removing " + el)
+            os.remove(os.path.join(path_dst, el))
 
 def main():
-    print(url_slideshow)
+    print(path_to_drive)
     subprocess.Popen([chrome, "--kiosk", url_slideshow, "--incognito"])
     time.sleep(1)
-    subprocess.Popen([chrome, "--kiosk", "--new-window", url_concierge, "--incognito", "--user-data-dir"])
+    subprocess.Popen([firefox, url_concierge])
+    remove_images(path_to_drive, "images")
     copy_images(path_to_drive, "images") # force copy images at startup
 
     while True:
         generate_page.main()
-        time.sleep(1800)  # update every 30 minutes
+        time.sleep(5) # update every 30 minutes
         copy_images(path_to_drive, "images")
-
-
+        remove_images(path_to_drive, "images")
+		
 if __name__ == '__main__':
     main()
